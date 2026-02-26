7f7c5e11e35b9b9df5d0f7809ed6c87ac239b68d
Niko <sirduckaltemail@gmail.com>
2026-02-26 10:18:17 -0600

    Refactor PowerShell script for MediaPlayer playback

--- a.ps1
+++ a.ps1
@@ -1,19 +1,38 @@
+# If not running in STA mode, relaunch self in STA (required for MediaPlayer)
+if ([System.Threading.Thread]::CurrentThread.ApartmentState -ne 'STA') {
+    $script = $MyInvocation.MyCommand.Path
+    if ($script) {
+        Start-Process powershell.exe -ArgumentList "-STA -WindowStyle Hidden -ExecutionPolicy Bypass -File `"$script`"" -WindowStyle Hidden
+    } else {
+        $tmp = "$env:TEMP\__logout_sta.ps1"
+        $MyInvocation.ScriptName | Out-Null
+        $content = @'
 Add-Type -AssemblyName presentationCore
-
-$url = "https://nikowoo.github.io/MUSIC/gecs\2.mp3"
-
-$mediaPlayer = New-Object System.Windows.Media.MediaPlayer
-$mediaPlayer.Open([uri]$url)
-$mediaPlayer.Play()
-Start-Sleep -Seconds 0
-
-$duration = $mediaPlayer.NaturalDuration
-if ($duration.HasTimeSpan) {
-    Start-Sleep -Seconds ([math]::Ceiling($duration.TimeSpan.TotalSeconds))
-} else {
-    Start-Sleep -Seconds 30 
+$url = "https://nikowoo.github.io/MUSIC/gecs.mp3"
+$mp = New-Object System.Windows.Media.MediaPlayer
+$mp.Open([uri]$url)
+$mp.Play()
+Start-Sleep -Seconds 2
+$d = $mp.NaturalDuration
+if ($d.HasTimeSpan) { Start-Sleep -Seconds ([math]::Ceiling($d.TimeSpan.TotalSeconds)) } else { Start-Sleep -Seconds 30 }
+$mp.Stop()
+$mp.Close()
+logoff
+'@
+        $content | Set-Content $tmp -Encoding UTF8
+        Start-Process powershell.exe -ArgumentList "-STA -WindowStyle Hidden -ExecutionPolicy Bypass -File `"$tmp`"" -WindowStyle Hidden
+    }
+    exit
 }
 
-$mediaPlayer.Stop()
-$mediaPlayer.Close()
+Add-Type -AssemblyName presentationCore
+$url = "https://nikowoo.github.io/MUSIC/gecs.mp3"
+$mp = New-Object System.Windows.Media.MediaPlayer
+$mp.Open([uri]$url)
+$mp.Play()
+Start-Sleep -Seconds 2
+$d = $mp.NaturalDuration
+if ($d.HasTimeSpan) { Start-Sleep -Seconds ([math]::Ceiling($d.TimeSpan.TotalSeconds)) } else { Start-Sleep -Seconds 30 }
+$mp.Stop()
+$mp.Close()
 logoff
